1226dbe8720e2104141e07b611ce951d4545e44c
somehuman <->
2023-02-19 17:35:48 +0300

    A-G

--- test.cpp
+++ test.cpp
@@ -2,16 +2,8 @@
 #include<deque>
 #include<iostream>
 
-class d {
-  public:
-  static const int G = 2;
-};
-
-
-
 int main() {
-  std::cout << d::G;
-  std::vector<int> a;
-  std::cout << a.size();
+  std::vector<int> aaa;
+  std::cout << aaa.size();
   return 0;
 }
