de995f5e5e0e9c89cb57850844e33e162b3ba435
somehuman <->
2023-04-23 10:47:01 +0300

    чтото

--- 2sem5/A.cpp
+++ 2sem5/A.cpp
@@ -1,23 +1,19 @@
 #include<iostream>
 #include<vector>
 #include<string>
-// КМП
+// MUHAHA
 
 int main() {
   std::string pat;
   std::string s;
   std::cin >> s >> pat;
   std::string a (pat + "0" + s);
-  std::vector<int> ans(a.size());
-  ans[s.size()] = 0;
-  int patsize = pat.size();
-  for (int i = patsize; i < a.size(); ++i) {
-    ans[i] = a[ans[i - 1]] == s[ans[i - 1]] ? ans[i - 1] + 1 : 0;
+  std::vector<size_t> pref(a.size(), 0);
+  for (size_t i = 1; i < a.size(); ++i) {
+    pref[i] = a[i] == pat[pref[i - 1]] ? pref[i - 1] + 1 : (pref[i - 1] == 0 ? 0 : (a[i] == pat[pref[pref[i - 1] - 1]] ? pref[i] = pref[pref[i - 1] - 1] + 1 : 0));
   }
-  for (int i = patsize; i < a.size(); ++i) {
-    if (ans[i] == patsize) {
-      std::cout << i << "\n";
-    }
+  for (size_t i = 0; i < a.size(); ++i) {
+    std::cout << (pref[i] == pat.size() ? std::to_string(i - 2 * pat.size()) + "\n" : "");
   }
   return 0;
 }
